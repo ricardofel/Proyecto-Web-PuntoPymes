bd9dd99aa6748fc5f2692973fa8d8edf82cda755
jdcamargo2 <jdiegocamargo2@gmail.com>
2025-11-30 00:56:52 -0500

    delete source

--- source/reportes_dashboards/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReportesDashboardsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reportes_dashboards'
--- source/reportes_dashboards/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def reporte_view(request):
-    return render(request, "reportes_dashboards/reporte.html")
