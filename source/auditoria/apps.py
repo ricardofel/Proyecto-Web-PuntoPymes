51a0f7a7145474f68c5d94b080a5d247a2d67b52
jdcamargo2 <jdiegocamargo2@gmail.com>
2026-01-10 11:17:45 -0500

    Auditoria: completo

--- source/auditoria/apps.py
+++ source/auditoria/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class AuditoriaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auditoria'
+
+    def ready(self):
+        # ¡ESTO ES CRÍTICO! Sin esto, el espía duerme.
+        import auditoria.signals
--- source/auditoria/models.py
+++ source/auditoria/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-"""
-Modelos de la app auditoria (opcional):
-
-- LogAuditoria
-
-Ver diccionario de datos y diseño técnico en:
-docs/Arquitectura_Estructura_TalentTrack.md
-"""
